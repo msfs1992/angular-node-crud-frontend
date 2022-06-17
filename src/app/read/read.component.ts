85d9a4ee5fdd52e58c7348019e0265dcebee97b7
msfs1992 <msfs1992@gmail.com>
2022-06-17 01:54:22 -0300

    v1

---
+++ src/app/read/read.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AnyForUntypedForms } from '@angular/forms';
+import { ApiserviceService } from '../apiservice.service';
+
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.css']
+})
+export class ReadComponent implements OnInit {
+
+  constructor(private service:ApiserviceService) { }
+
+  readData:any;
+  successmsg:any;
+
+  ngOnInit(): void {
+    this.getAllData();
+  }
+
+  //get delete id
+  deleteID(id:any)
+  {
+    console.log(id, 'deleteid==>');
+    this.service.deleteData(id).subscribe((res)=>{
+      console.log(res, 'deleteres==>');
+      this.successmsg = res.message;
+      this.getAllData();
+    });
+  }
+  getAllData()
+  {
+    this.service.getAllData().subscribe((res)=>{
+      console.log(res, "res==>");
+      this.readData = res.data;
+    });
+  }
+}
